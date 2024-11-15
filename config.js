37bbdb04d987759d3c0804f2a3ded9c4e915112b
DamianFerArg <DamianFerArg@users.noreply.github.com>
2024-11-15 15:48:00 +0000

    deploy: e1f12969ec0a9c31fcff9bce3d0f1cce47c0686e

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBvluKIuZRR3CDlGeJSa6qYF0pAdgCpnBE',
+  authDomain: 'proyectclothes-b6e88.firebaseapp.com',
+  projectId: 'proyectclothes-b6e88',
+  storageBucket: 'proyectclothes-b6e88.appspot.com',
+  messagingSenderId: '973366577223',
+  appId: '1:973366577223:web:66403424635d1300de4962',
+  measurementId: 'G-E7SX5N778J'
+};
